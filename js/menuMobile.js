99e5e39146e5b2d44a667ac0e632f5a6f010094a
Emanuel Vitor Martins Prates <emanuelvitorprates@gmail.com>
2023-11-05 15:36:38 -0300

    Initial commit

---
+++ js/menuMobile.js
@@ -0,0 +1,14 @@
+const buttonMenu = document.querySelector('.button__menu--mobile')
+const menuMobile = document.querySelector('.cabecalho__menu--mobile')
+let isMenuOpen = false
+
+
+
+buttonMenu.addEventListener('click', function () {
+    isMenuOpen ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible')
+
+    isMenuOpen ? buttonMenu.children[0].setAttribute('src', '../assets/menu.svg ') : buttonMenu.children[0].setAttribute('src', '../assets/close.svg ')
+
+    isMenuOpen = !isMenuOpen
+
+})
